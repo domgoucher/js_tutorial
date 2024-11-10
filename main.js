794f3a34659768e0096a80dd7bf8661e5cbc6128
Dom Goucher <dgoucher@gmail.com>
2024-11-10 20:48:28 +0100

    Add palindrome tester page

---
+++ main.js
@@ -0,0 +1,10 @@
+let Phrase = require("domgoucher-palindrome");
+
+let string = prompt("Please enter a string for palindrome testing:");
+let phrase = new Phrase(string);
+
+if (phrase.palindrome()) {
+  alert(`"${phrase.content}" is a palindrome!`);
+} else {
+  alert(`"${phrase.content}" is a not palindrome!`);
+}
